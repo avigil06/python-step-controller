df85b1f6b1f1d2570d79e3fa39cb521f4f9af90b
Andrew <avigil06@gmail.com>
2019-12-22 13:08:17 -0500

    disable motor1

--- main.py
+++ main.py
@@ -15,25 +15,24 @@ GPIO.setmode(GPIO.BCM)
 # GPIO10,GPIO9,GPIO11,GPI25
 # StepPins = [17,18,27,22]
 
-motor1 = Motor(18, 17, 27, 22)
+# motor1 = Motor(18, 17, 27, 22)
 motor2 = Motor(12, 13, 19, 26)
 
 
+steps_to_rotate = 150
 # Start main loop
 button = Button(14, 15)
 while True:
     if button.is_forward():
         print("forward")
-        motor1.forward(128)
-        time.sleep(1)
-        motor2.forward(128)
+        # motor1.forward(128)
+        motor2.forward(steps_to_rotate)
     elif button.is_backward():
         print("backward")
-        motor1.backward(128)
-        time.sleep(1)
-        motor2.backward(128)
+        # motor1.backward(128)
+        motor2.backward(steps_to_rotate)
     else:
-        motor1.reset()
+        # motor1.reset()
         motor2.reset()
 
 try:
@@ -42,6 +41,6 @@ except:
   print("Failed")
 finally:
     #cleaning up and setting pins to low again (motors can get hot if you wont)
-    motor1.reset()
+    # motor1.reset()
     motor2.reset()
     GPIO.cleanup()
